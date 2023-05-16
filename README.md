339503be2ef49a77f4201efe501eac234b624a05
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-16 13:56:28 +0200

    add addEbenListener

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AsynchroneExercice1
+
+https://developer.mozilla.org/fr/docs/Web/API/Fetch_API/Using_Fetch
--- script.js
+++ script.js
@@ -2,7 +2,22 @@ let body = document.createElement('body');
 document.documentElement.appendChild(body);
 
 let buttonJson = document.createElement('button');
-let message = document.createTextNode("click me");
-buttonJson.appendChild(message);
+let message = document.createTextNode("click me for read my list");
 buttonJson.classList.add('buttonJson');
+buttonJson.appendChild(message);
 body.appendChild(buttonJson);
+
+
+buttonJson.addEventListener("click", (e) =>{
+    fetch('data.json')
+    .then(response => response.json())
+    .then(data => {
+        body.innerHTML = ''; // Clear the existing list
+        data.forEach(item => {
+          const listItem = document.createElement('li');
+          listItem.textContent = item;
+          body.appendChild(listItem);
+        });
+      })
+      .catch(error => console.log(error));
+  });
